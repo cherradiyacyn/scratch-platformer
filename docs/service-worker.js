de134246ffdf7104bc6f779b5a8d1a24849d20c6
cherradiyacyn <cherradiyacyn@gmail.com>
2025-03-24 08:24:59 +0000

    More changes

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -2,10 +2,13 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("pwa-cache").then((cache) => {
       return cache.addAll([
-        "/",
-        "/index.html",
-        "/android/android-launchericon-192-192.png",
-        "/android/android-launchericon-512-512.png",
+        "/scratch-platformer/",
+        "/scratch-platformer/index.html",
+        "/scratch-platformer/manifest.json",
+        "/scratch-platformer/android/android-launchericon-192-192.png",
+        "/scratch-platformer/android/android-launchericon-512-512.png",
+        "/scratch-platformer/ios/180.png",
+        "/scratch-platformer/windows11/Square150x150Logo.scale-200.png",
       ]);
     })
   );
